3aa599ec16eaae3548bdc45da628b4a29c2af167
Yusuf Çınar Özmen <marulumbenaq@gmail.com>
2021-04-22 17:23:27 +0300

    Add files via upload

---
+++ Assets/Cube.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Discord;
+using System;
+
+public class Cube : MonoBehaviour
+{
+    public int jumpAmount = 0;
+
+    Discord.Discord discord;
+    Discord.ActivityManager activityManager;
+
+    private void Start()
+    {
+        discord = new Discord.Discord(834754357056634880, (UInt64)Discord.CreateFlags.Default);
+        activityManager = discord.GetActivityManager();
+        UpdatePresence();
+    }
+
+    void Update()
+    {
+        discord.RunCallbacks();
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 250f);
+            jumpAmount++;
+            UpdatePresence();
+        }
+    }
+
+    void UpdatePresence()
+    {
+        activityManager.UpdateActivity(new Discord.Activity {
+            Details = "Küp zıplatılıyor...",
+            State = jumpAmount.ToString() + " defa zıplatıldı.",
+            Assets = {
+                LargeImage = "potato",
+                LargeText = "Potato uwu :>"
+            }
+        }, (res) => { });
+    }
+}
